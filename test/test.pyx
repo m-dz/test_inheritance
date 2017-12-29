123bc1ff8546c7bdb9905675ec8bca3a97763655
m-dz <mat.dziedzic@gmail.com>
2017-12-29 01:13:28 +0100

    Working single file with cdef classes

--- test/test.pyx
+++ test/test.pyx
@@ -1,22 +1,29 @@
 # test.pyx
 
-class Superclass:
+cdef class Superclass:
 
-    def __init__(self, a, b):
+    cdef int a
+    cdef int b
+    cdef int c
+    cdef int d
+
+    def __init__(self, a, b, c=0, d=0):
         self.a = a
         self.b = b
-        self.c = None
+        self.c = c
+        self.d = d
 
 
 # subclass_A.pyx
 
 # from superclass import Superclass
 
-class SubclassA(Superclass):
+cdef class SubclassA(Superclass):
+
     def __init__(self, a=0, b=0):
         super(SubclassA, self).__init__(a=a, b=b)
 
-    def do_that(self, c, d=None):
+    cpdef int do_that(self, int c, int d=0):
         self.c = c
         self.d = d
         print(self.a + self.b + self.c)
@@ -27,11 +34,12 @@ class SubclassA(Superclass):
 
 # from superclass import Superclass
 
-class SubclassB(Superclass):
+cdef class SubclassB(Superclass):
+
     def __init__(self, a=0, b=0):
         super(SubclassB, self).__init__(a=a, b=b)
 
-    def do_that(self, c, d=None):
+    cpdef do_that(self, int c, int d):
         self.c = c
         self.d = d
         print(self.a + self.b + self.c + self.d)
