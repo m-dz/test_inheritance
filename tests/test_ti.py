ecd09477e6905d728903016707a1af3cd72c07e3
m-dz <mat.dziedzic@gmail.com>
2017-12-31 20:04:22 +0100

    Add exception tests
    New tests not passing in this version.

--- tests/test_ti.py
+++ tests/test_ti.py
@@ -13,3 +13,12 @@ def test_run_me():
     this_B = SubclassB(a=1, b=2)
     ret_B = this_B.do_that(c=3, d=4)
     assert(ret_B == 10)
+
+# This does not raise any exception in this version
+def test_passes():
+    this_A = SubclassA(a=1, b=2)
+    with pytest.raises(Exception) as e_info:
+        this_A.do_that(c=3, d=4)
+
+    with pytest.raises(Exception) as e_info:
+        this_A.do_that(3, 4)
