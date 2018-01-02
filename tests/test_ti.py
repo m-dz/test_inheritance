3b25df70894156f0d5a9724986f91f29983d0741
m-dz <mat.dziedzic@gmail.com>
2018-01-02 19:46:08 +0100

    Split tests
    All tests passing.

--- tests/test_ti.py
+++ tests/test_ti.py
@@ -4,20 +4,29 @@
 from ti import SubclassA, SubclassB
 import pytest
 
-def test_run_me():
+def test_A_pass():
 
     this_A = SubclassA(a=1, b=2)
     ret_A = this_A.do_that(3)
     assert(ret_A == 6)
 
+def test_B_pass():
+
     this_B = SubclassB(a=1, b=2)
     ret_B = this_B.do_that(c=3, d=4)
     assert(ret_B == 10)
 
-def test_passes():
+def test_A_raises_1():
     this_A = SubclassA(a=1, b=2)
     with pytest.raises(Exception) as e_info:
         this_A.do_that(c=3, d=4)
 
+def test_A_raises_2():
+    this_A = SubclassA(a=1, b=2)
     with pytest.raises(Exception) as e_info:
         this_A.do_that(3, 4)
+
+def test_A_raises_3():
+    this_A = SubclassA(a=1, b=2)
+    with pytest.raises(Exception) as e_info:
+        this_A.do_that(3, d=4)
